35bfa4fa748bd1b048635daefe86fb7dacc01c8f
rabia <rabiawadhwa@gmail.com>
2021-10-13 10:25:42 +0530

    Get data function implemented

--- training.py
+++ training.py
@@ -1,4 +1,5 @@
-from utils.common_utils import read_config
+from src.utils.common_utils import read_config
+from src.utils.data_mgmt import get_data
 import argparse
 
 
@@ -6,6 +7,9 @@ def training(config_path):
     config = read_config(config_path)
     print(config)
 
+    validation_datasize = config["params"]["validation_datasize"]
+    print(validation_datasize)
+    (X_train,y_train),(X_valid,y_valid),(X_test,y_test) = get_data(validation_datasize)
 
 if __name__ == '__main__':
      
